1171c4e8902ec08d81bf96b4efa3ae8a74b9e87a
Mathias Alexander <mathias.alexander@irhythmtech.com>
2024-08-06 08:49:08 -0700

    Database integration and listing of tasks in tables

--- ius_time/web_ui.py
+++ ius_time/web_ui.py
@@ -1,23 +1,70 @@
-from fasthtml import common as fh
+# ruff: noqa: F403, F405
+from fasthtml.common import *
 
-from ius_time.db import DB_PATH
+from ius_time.db import DB_PATH, Status
 
-app, rt, tasks, Task = fh.fast_app(db_file=DB_PATH, hdrs=fh.picolink,
-                                   id=int,
-                                   name=str,
-                                   start_time=float,
-                                   end_time=float,
-                                   total_time=float,
-                                   categorty=str,
-                                   status=str,
-                                   pk="id")
+db = database(DB_PATH)
+tasks = db.t.tasks
+if tasks not in db.t:
+    tasks.create(id=int,
+                 name=str,
+                 start_time=float,
+                 end_time=float,
+                 total_time=float,
+                 categorty=str,
+                 status=str,
+                 pk="id",
+                 not_null=["id", "name", "start_time", "category", "status"])
+Task = tasks.dataclass()
+
+app = FastHTMLWithLiveReload(hdrs=(picolink, SortableJS()))
+rt = app.route
+
+
+def mk_active_row(task: Task):
+    return Tr(Td(task.name), Td(task.start_time), Td(task.category), Td(Button("End Task")), id=f"active-task-{task.id}")
+
+
+def mk_completed_row(task: Task):
+    return Tr(Td(task.name), Td(task.start_time), Td(task.end_time), Td(task.total_time), Td(task.category), id=f"complete-task-{task.id}")
+
+
+def active_tasks_table():
+    return Table(
+        Thead(Tr(Th("Name"), Th("Start Time"), Th("Category"), Th("Action"))),
+        Tbody(
+            *(mk_active_row(Task(**task)) for task in
+              db.q(f'select * from {tasks} where {tasks.c.status} = "{Status.ACTIVE.value}"'))
+        )
+    )
+
+
+def completed_tasks_table():
+    return Table(
+        Thead(Tr(Th("Name"), Th("Start Time"), Th("End Time"), Th("Total Time"), Th("Category"))),
+        Tbody(
+            *(mk_completed_row(Task(**task)) for task in
+              db.q(f'select * from {tasks} where {tasks.c.status} = "{Status.COMPLETE.value}"'))
+        )
+    )
 
 
 @rt("/")
 def get():
-    return fh.Titled("IUS Time",
-                     fh.Div(fh.P("Hello, World")))
+    return Titled(
+        "IUS Time",
+
+        Div(
+            H1("Active Tasks"),
+            active_tasks_table()
+        ),
+        Div(
+            H1("Completed Tasks"),
+            completed_tasks_table()
+        )
+
+    )
 
 
-def run():
-    fh.serve(appname=".", port=5001)
+if __name__ == "__main__":
+    serve(host="127.0.0.1")
