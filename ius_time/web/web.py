4aff1b44b3e8329dafb3c09a67273080e1a6a632
Mathias Alexander <mathias@mathias-alexander.com>
2025-04-26 10:21:42 -0700

    Styled and functional simple web UI

--- ius_time/web/web.py
+++ ius_time/web/web.py
@@ -1,3 +1,5 @@
+import urllib.parse as urlp
+
 from fastapi import FastAPI, Request, status
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -7,21 +9,22 @@ from sqlmodel import select
 from ius_time.db import DEFAULT_DB_PATH, Session, Status, Task, TaskManager
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
 task_manager = TaskManager(DEFAULT_DB_PATH)
 
 
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
+async def index(request: Request, message: str = ""):
     active_tasks = task_manager.list_active()
     complete_tasks = task_manager.list_complete()
     return templates.TemplateResponse(
         request=request,
         name="index.html",
-        context={"active": active_tasks, "complete": complete_tasks},
+        context={
+            "active": active_tasks,
+            "complete": complete_tasks,
+            "message": urlp.unquote(message),
+        },
     )
 
 
@@ -29,29 +32,29 @@ async def index(request: Request):
 async def start_task(request: Request):
     form_data = await request.form()
     name = form_data["new-task-name"]
+    if name == "":
+        return RedirectResponse(f"/?message={urlp.quote('Task must have a name')}")
     category = form_data["new-task-category"]
-    task_manager.start_task(name, category)
-    return RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
-    # with Session(task_manager.db_engine) as session:
-    #     new_task = session.exec(
-    #         select(Task).where(
-    #             Task.name == name,
-    #             Task.category == category,
-    #             Task.status == Status.ACTIVE,
-    #         )
-    #     ).one()
-    # return new_task.model_dump_json(exclude_none=True)
+    try:
+        task_manager.start_task(name, category)
+        return RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
+    except Exception as e:
+        return RedirectResponse(
+            f"/?message={urlp.quote(str(e))}",
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
 @app.get("/end/{task_name}", response_class=JSONResponse)
 async def end_task(request: Request, task_name: str):
-    task_manager.end_task(task_name)
-    return RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
-    # with Session(task_manager.db_engine) as session:
-    #     completed_task = session.exec(
-    #         select(Task).where(Task.name == task_name, Task.status == Status.COMPLETE)
-    #     ).one()
-    # return completed_task.model_dump_json()
+    try:
+        task_manager.end_task(task_name)
+        return RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
+    except Exception as e:
+        return RedirectResponse(
+            f"/?message={urlp.quote(str(e))}",
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
 if __name__ == "__main__":
