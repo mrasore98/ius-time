5561495afab1197d36cbadb2327064a145d6f69f
Mathias Alexander <mathias@mathias-alexander.com>
2025-04-26 20:39:35 -0700

    Add web command to CLI

--- ius_time/cli/main.py
+++ ius_time/cli/main.py
@@ -1,4 +1,8 @@
-""" Command line interface for ius_time. """
+"""Command line interface for ius_time."""
+
+import threading
+import webbrowser
+
 import typer
 
 from ius_time import DEFAULT_FILTER, console
@@ -13,22 +17,17 @@ app.add_typer(end_tasks.app, name="end", help="End active tasks.")
 
 
 @app.command(help="Start a new task.")
-def start(task_name: str,
-          category: str = typer.Option(
-              "Misc",
-              "-c",
-              "--category")
-          ):
+def start(task_name: str, category: str = typer.Option("Misc", "-c", "--category")):
     tm.start_task(task_name, category=category)
 
 
-@app.command(help="Sum the amount of time spent on your tasks. Only calculated for completed tasks")
+@app.command(
+    help="Sum the amount of time spent on your tasks. Only calculated for completed tasks"
+)
 def total(
-        filter_: FilterEnum = DEFAULT_FILTER,
-        category: str = typer.Option(None,
-                                     "-c",
-                                     "--category")
-        ):
+    filter_: FilterEnum = DEFAULT_FILTER,
+    category: str = typer.Option(None, "-c", "--category"),
+):
     rows = tm.sum_task_times(filter_, category=category)
     table_name = "Total Time"
     if category is not None:
@@ -37,6 +36,25 @@ def total(
         table_name += f" ({filter_})"
     table = total_rows_as_table(rows, table_name)
     console.print(table)
+
+
+@app.command()
+def web():
+    LOOPBACK_ADDR = "127.0.0.1"
+    PORT = 5001
+
+    def open_browser():
+        webbrowser.open_new(f"http://{LOOPBACK_ADDR}:{PORT}")
+
+    try:
+        import uvicorn
+
+        from ius_time.web import web_app
+
+        threading.Timer(1, open_browser).start()
+        uvicorn.run(web_app, host=LOOPBACK_ADDR, port=PORT)
+    except ImportError:
+        console.print('[error]Error: "web" extra is not installed.')
 
 
 def main():
--- ius_time/web/__init__.py
+++ ius_time/web/__init__.py
@@ -1,1 +1,4 @@
+from .web import app as web_app
+
+__all__ = ["web_app"]
 
