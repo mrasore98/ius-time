58b96ff344549dbba5a157f76f825e6ac1a9bbaa
Mathias Alexander <mathias@mathias-alexander.com>
2025-05-02 16:16:21 -0700

    Start and end return the Task

--- ius_time/db.py
+++ ius_time/db.py
@@ -13,9 +13,11 @@ from ius_time.utils import TaskTime, datetime_format, datetime_pst, ius_theme
 DEFAULT_DB_PATH = Path(__file__).parent.parent.resolve() / "ius-tasks.db"
 DEFAULT_DB_ENGINE = create_engine(f"sqlite:///{DEFAULT_DB_PATH}")
 
+
 class Status(StrEnum):
     ACTIVE = "Active"
     COMPLETE = "Complete"
+
 
 class Task(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -25,6 +27,7 @@ class Task(SQLModel, table=True):
     total_time: timedelta | None = None
     category: str
     status: Status = Status.ACTIVE
+
 
 class TaskManager:
     def __init__(self, db_path: Path = DEFAULT_DB_PATH):
@@ -40,53 +43,69 @@ class TaskManager:
         except Exception as e:
             self.console.print(f"[error]Error creating task table: {e}")
             return False
-    
+
     # START FUNCTIONS
-    def start_task(self, task_name: str, category: str = "Misc"):
+    def start_task(self, task_name: str, category: str = "Misc") -> Task | None:
         try:
             with Session(self.db_engine) as session:
                 task = Task(name=task_name, category=category)
                 session.add(task)
                 session.commit()
                 session.refresh(task)
-            self.console.print(f"Starting task [info]{task.name}[/] at [info]{task.start_time.strftime(datetime_format)}[/]")
+
+            self.console.print(
+                f"Starting task [info]{task.name}[/] at [info]{task.start_time.strftime(datetime_format)}[/]"
+            )
+            return task
         except Exception as e:
-            self.console.print(f"[error]Could not start task [info]{task_name}[/info]! \n{e}[/error]")
-            
+            self.console.print(
+                f"[error]Could not start task [info]{task_name}[/info]! \n{e}[/error]"
+            )
+            return None
+
     # END FUNCTIONS
     def _set_end_attributes(self, task: Task):
         task.end_time = datetime_pst.now()
         task.total_time = task.end_time - datetime_pst.from_datetime(task.start_time)
         task.status = Status.COMPLETE
-        
+
     def _print_end_successful(self, task: Task):
-        self.console.print(f"[success]Task [info]{task.name}[/info] ended after [info]{TaskTime(task.total_time.total_seconds())!s}[/info] :100:")       
-    
-    def end_task(self, task_name: str) -> bool:
+        self.console.print(
+            f"[success]Task [info]{task.name}[/info] ended after [info]{TaskTime(task.total_time.total_seconds())!s}[/info] :100:"
+        )
+
+    def end_task(self, task_name: str) -> Task | None:
         try:
             with Session(self.db_engine) as session:
                 task: Task | None = session.exec(
-                    select(Task).where(Task.name == task_name, Task.status == Status.ACTIVE)
+                    select(Task).where(
+                        Task.name == task_name, Task.status == Status.ACTIVE
+                    )
                 ).first()
                 if task:
                     self._set_end_attributes(task)
                     session.commit()
                     session.refresh(task)
                     self._print_end_successful(task)
-                    return True
+                    return task
                 else:
-                    self.console.print(f"[error]{task_name} is not an Active Task[/error]")
-                    return False
+                    self.console.print(
+                        f"[error]{task_name} is not an Active Task[/error]"
+                    )
+                    return None
         except Exception as e:
-            self.console.print(f"[error]Could not end task [info]{task_name}[/info]: {e}[/error]")
-            return False
+            self.console.print(
+                f"[error]Could not end task [info]{task_name}[/info]: {e}[/error]"
+            )
+            return None
 
-    def end_last(self) -> bool:
+    def end_last(self) -> Task | None:
         try:
             with Session(self.db_engine) as session:
                 last_task_name = session.exec(
                     select(Task.name).where(
-                        Task.start_time == (
+                        Task.start_time
+                        == (
                             select(func.max(Task.start_time))
                             .where(Task.status == Status.ACTIVE)
                             .scalar_subquery()
@@ -97,26 +116,28 @@ class TaskManager:
                 return self.end_task(last_task_name)
             else:
                 self.console.print("[error]No active tasks to end![/error]")
-                return False
+                return None
         except Exception as e:
             self.console.print(f"[error]Could not end last task: {e}[/error]")
-            return False
-    
+            return None
+
     def end_all_active(self) -> bool:
         try:
             with Session(self.db_engine) as session:
-               active_tasks = session.exec(
-                   select(Task).where(Task.status == Status.ACTIVE)
-               ).all()
-               for task in active_tasks:
-                   self._set_end_attributes(task)
-               session.commit()
-            self.console.print(f"[success]Ended [info]{len(active_tasks)}[/info] active tasks at [info]{datetime_pst.now().strftime(datetime_format)}[/info]!")
+                active_tasks = session.exec(
+                    select(Task).where(Task.status == Status.ACTIVE)
+                ).all()
+                for task in active_tasks:
+                    self._set_end_attributes(task)
+                session.commit()
+            self.console.print(
+                f"[success]Ended [info]{len(active_tasks)}[/info] active tasks at [info]{datetime_pst.now().strftime(datetime_format)}[/info]!"
+            )
             return True
         except Exception as e:
             self.console.print(f"[error]Could not end all tasks: {e}[/error]")
             return False
-            
+
     # LIST FUNCTIONS
     def list_active(self, filter_: FilterEnum = FilterEnum.MONTH) -> list[Task]:
         expression = select(Task).where(Task.status == Status.ACTIVE)
@@ -126,7 +147,7 @@ class TaskManager:
         with Session(self.db_engine) as session:
             active_tasks = session.exec(expression).all()
             return list(active_tasks)
-            
+
     def list_complete(self, filter_: FilterEnum = FilterEnum.MONTH) -> list[Task]:
         expression = select(Task).where(Task.status == Status.COMPLETE)
         if filter_ != FilterEnum.NONE:
@@ -135,7 +156,7 @@ class TaskManager:
         with Session(self.db_engine) as session:
             complete_tasks = session.exec(expression).all()
             return list(complete_tasks)
-    
+
     def list_all(self, filter_: FilterEnum = FilterEnum.MONTH) -> list[Task]:
         expression = select(Task)
         if filter_ != FilterEnum.NONE:
@@ -144,10 +165,14 @@ class TaskManager:
         with Session(self.db_engine) as session:
             all_tasks = session.exec(expression).all()
             return list(all_tasks)
-            
+
     # TOTAL FUNCTIONS
-    def sum_task_times(self, filter_: FilterEnum = FilterEnum.MONTH, category: str | None = None) -> list[tuple[str, int]]:
-        expression = select(Task.category, Task.total_time).where(Task.status == Status.COMPLETE)
+    def sum_task_times(
+        self, filter_: FilterEnum = FilterEnum.MONTH, category: str | None = None
+    ) -> list[tuple[str, int]]:
+        expression = select(Task.category, Task.total_time).where(
+            Task.status == Status.COMPLETE
+        )
         if category:
             expression = expression.where(Task.category == category)
         if filter_ != FilterEnum.NONE:
@@ -163,3 +188,4 @@ class TaskManager:
             for group, rows in grouped_times
         ]
         return sorted(results, key=lambda x: x[1], reverse=True)
+
